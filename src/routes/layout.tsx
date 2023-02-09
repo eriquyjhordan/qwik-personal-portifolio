99ec968516b810a1efb6fbd379f2d0b13ccfc861
Eriquy Jhordan <eriquy@gmail.com>
2023-02-09 17:09:58 -0300

    feat: lottie animation of editable website

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -11,6 +11,8 @@ export default component$(() => {
         <HeroSection />
         <section>
           <Slot />
+          {/* <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+          <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_kU6c7JDFKn.json" background="transparent" speed="1" style="width: 500px; height: 500px;" loop autoplay></lottie-player> */}
         </section>
       </main>
     </>
